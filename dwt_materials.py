0dbec923a97adcaab1fe2fc5f1edcda7c2c7ffa0
NegativeIndex <daiweitao@yahoo.com>
2019-11-04 12:27:10 -0600

    Add Sphinx help files

--- dwt_materials.py
+++ dwt_materials.py
@@ -1,3 +1,8 @@
+"""Several funcitons to calculate permittivity and refractive index
+from a Meep medium.
+
+"""
+
 import meep as mp
 import numpy as np
 import math
@@ -9,12 +14,19 @@ import sys
 ######################################
 
 def symmetric_matrix_from_2vectors(v1=[1,1,1],v2=[0,0,0]):
+    """Generate a 3x3 symmetric matrix from two 3-element arrays with
+    diagonal and non-diagonal values
+
+    """
     a,b,c=v1
     u,v,w=v2
     return np.complex128(np.matrix([[a,u,v],[u,b,w],[v,w,c]]))
 
 # reduce matrix to a scalar if it is a constant matrix
 def reduce_matrix_to_scalar(M):
+    """ Reduce a matrix to a scalar if it is a constant matrix
+
+    """
     a=M.diagonal().mean()
     if (M.shape[0]==M.shape[1]) and np.allclose(M, a*np.eye(M.shape[0])):
         return a
@@ -22,6 +34,11 @@ def reduce_matrix_to_scalar(M):
         return None
 
 def get_Permittivty_from_Medium(freq,medium):
+    """Calculate the permittivity from a Meep medium given the
+    frequency. The return value is a 3x3 matrix. I considered the
+    conductivity, Lorentian susceptibility and Drude susceptibility.
+
+    """
     sigma=symmetric_matrix_from_2vectors(medium.D_conductivity_diag)
     prod0=np.identity(3)+1j*sigma/(2*math.pi*freq)
     
@@ -43,9 +60,14 @@ def get_Permittivty_from_Medium(freq,medium):
 
 
 def get_refractive_index(freq,medium):
-	epsi_t=get_Permittivty_from_Medium(freq,medium)
-	epsi=reduce_matrix_to_scalar(epsi_t)
-	return cmath.sqrt(epsi)
+    """Calculate the refractive index from a Meep medium given the
+    frequency. The return value is a complex number. I assume the
+    permeabilit is 1 and the permittiviyt is isotropic.
+
+    """
+    epsi_t=get_Permittivty_from_Medium(freq,medium)
+    epsi=reduce_matrix_to_scalar(epsi_t)
+    return cmath.sqrt(epsi)
 
 ########################################################
 ########################################################
@@ -73,6 +95,7 @@ In295_susc = [mp.DrudeSusceptibility(frequency=In295_frq0,
                                   gamma=In295_gam0, sigma=In295_sig0),
        ]
 
+#:Personal material, Indium at 295K  
 In295 = mp.Medium(epsilon=3.363, E_susceptibilities=In295_susc, 
                valid_freq_range=metal_range)
 
@@ -90,35 +113,7 @@ In4p2_susc = [mp.DrudeSusceptibility(frequency=In4p2_frq0,
                                   gamma=In4p2_gam0, sigma=In4p2_sig0),
        ]
 
+#:Personal material, Indium at 4.2K 
 In4p2 = mp.Medium(epsilon=1.000, E_susceptibilities=In4p2_susc, 
                valid_freq_range=metal_range)
 
-############################
-# palladium (Pd)
-############################
-Pd_plasma_frq = 9.72*eV_um_scale
-Pd_f0 = 0.330
-Pd_frq0 = 1e-10
-Pd_gam0 = 0.008*eV_um_scale
-Pd_sig0 = Pd_f0*Pd_plasma_frq**2/Pd_frq0**2
-Pd_f1 = 0.649
-Pd_frq1 = 0.336*eV_um_scale      # 3.690 um
-Pd_gam1 = 2.950*eV_um_scale
-Pd_sig1 = Pd_f1*Pd_plasma_frq**2/Pd_frq1**2
-Pd_f2 = 0.121
-Pd_frq2 = 0.501*eV_um_scale      # 2.475 um
-Pd_gam2 = 0.555*eV_um_scale
-Pd_sig2 = Pd_f2*Pd_plasma_frq**2/Pd_frq2**2
-Pd_f3 = 0.638
-Pd_frq3 = 1.659*eV_um_scale      # 0.747 um
-Pd_gam3 = 4.621*eV_um_scale
-Pd_sig3 = Pd_f3*Pd_plasma_frq**2/Pd_frq3**2
-Pd_f4 = 0.453
-Pd_frq4 = 5.715*eV_um_scale      # 0.217 um
-Pd_gam4 = 3.236*eV_um_scale
-Pd_sig4 = Pd_f4*Pd_plasma_frq**2/Pd_frq4**2
-
-Pd_susc = [mp.DrudeSusceptibility(frequency=Pd_frq0, 
-                                  gamma=Pd_gam0, sigma=Pd_sig0),]
-Pd = mp.Medium(epsilon=1.0, E_susceptibilities=Pd_susc, 
-               valid_freq_range=metal_range)
---
+++ index.rst
@@ -0,0 +1,19 @@
+Welcome to MeepMedium's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+MeepMedium module
+----------------------------------------------------------------
+.. automodule:: MeepMedium.dwt_materials
+         :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
